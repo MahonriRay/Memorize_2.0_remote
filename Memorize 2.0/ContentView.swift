2c579ccefe317fa7529dd284085077a0cdd5e585
MahonriRay <MahonriRay@byui.edu>
2022-12-28 01:55:50 -0700

    Initial Commit

---
+++ Memorize 2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize 2.0
+//
+//  Created by Mahonri Ray on 12/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize 2.0/Memorize_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_2_0App.swift
+//  Memorize 2.0
+//
+//  Created by Mahonri Ray on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
